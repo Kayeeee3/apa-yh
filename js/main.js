8c206bb585511a9cc0ff9c5db049a3151fa69579
Kayeeee3 <55bravogaya@gmail.com>
2025-08-22 00:39:28 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HABEDEE BOCIILL').split('')
+    const titles = ('LOPYUUUU BOCIILL').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
